e52620690ba6396d52ac2dc8f781aa99f55158f8
MAlngaawy <m.alngaawy@gmail.com>
2022-07-14 19:54:26 +0200

    change the code  functionality

--- js/app.js
+++ js/app.js
@@ -6,24 +6,14 @@ const addAChild = (content, sectionID) => {
   // create teh  li element and it's a tag child
   const listItemElement = document.createElement("li")
 
-  // access the related section
-  const relatedSection = document.getElementById(sectionID)
-
   // add the content to the li element
   listItemElement.innerHTML = content
-
-  // give a spicial id to teh navbar elemnt according to the section id
-  listItemElement.id = `navTo${sectionID}`
-
-  // add the created li element to the ul parent
-  navList.appendChild(listItemElement)
-
-  // add the click function that scroll to the section
-  listItemElement.addEventListener('click', () => {
-    relatedSection.scrollIntoView({behavior: "smooth"})
-
-  })
   
+  // give a spicial id to teh navbar elemnt according to the section id
+  listItemElement.dataset.select = `${sectionID}`
+  
+  // add the created li element to the ul parent
+  navList.appendChild(listItemElement)  
 }
 
 // create the navbar according to the sections
@@ -34,3 +24,29 @@ for(let i = 0; i < sections.length; i++) {
   addAChild(content, sectionID)
 }
 
+// access the nav items
+const items = document.querySelectorAll("li")
+
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    // access the related section in a variable
+    let relatedSection = document.getElementById(item.dataset.select)
+    
+    //scroll to the related section
+    relatedSection.scrollIntoView({behavior: "smooth"})
+
+    // remove active classes from all the section and add active class to the related section
+    sections.forEach(oneSection  => {
+      oneSection.className = ''
+    })
+    relatedSection.className = 'active'
+
+    // remove all active classes from all the items and add it to the clicked item
+    items.forEach(item => {
+      item.className = ''
+    })
+    item.className = 'active'
+
+  })
+
+})
