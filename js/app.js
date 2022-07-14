3800d5dc8e40d2fb6a482a855110476a63429373
MAlngaawy <m.alngaawy@gmail.com>
2022-07-14 15:07:47 +0200

    make the scroll with JS

--- js/app.js
+++ js/app.js
@@ -2,24 +2,30 @@
 const navList = document.getElementById("navbar__list")
 const sections = document.querySelectorAll("section")
 
-const addAChild = (content, theID) => {
+const addAChild = (content, sectionID) => {
   // create teh  li element and it's a tag child
   const listItemElement = document.createElement("li")
-  
+  const relatedSection = document.getElementById(sectionID)
   // add the content and the attr to the elements
   // anchorTag.href = `#${theID}`
   // anchorTag.innerHTML = content
 
   // add the a element to the li element and add the li element to the ul element
   listItemElement.innerHTML = content
+  listItemElement.id = `navTo${sectionID}`
+  listItemElement.addEventListener('click', () => {
+    console.log(sectionID)
+    relatedSection.scrollIntoView({behavior: "smooth"})
+
+  })
   navList.appendChild(listItemElement)
 }
 
 // create the navbar according to the sections
 for(let i = 0; i < sections.length; i++) {
   let content = sections[i].dataset.nav
-  let theID = sections[i].id
+  let sectionID = sections[i].id
 
-  addAChild(content, theID)
+  addAChild(content, sectionID)
 }
 
