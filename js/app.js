7847f35373e4030ebbba08478b6386dc11e851f6
MAlngaawy <m.alngaawy@gmail.com>
2022-07-14 14:31:35 +0200

    use let and const instead of vat

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ const sections = document.querySelectorAll("section")
 
 const addAChild = (content, theID) => {
   // create teh  li element and it's a tag child
-  var listItemElement = document.createElement("li")
-  var anchorTag = document.createElement("a")
+  const listItemElement = document.createElement("li")
+  const anchorTag = document.createElement("a")
   
   // add the content and the attr to the elements
   anchorTag.href = `#${theID}`
@@ -18,8 +18,8 @@ const addAChild = (content, theID) => {
 
 // create the navbar according to the sections
 for(let i = 0; i < sections.length; i++) {
-  var content = sections[i].dataset.nav
-  var theID = sections[i].id
+  let content = sections[i].dataset.nav
+  let theID = sections[i].id
 
   addAChild(content, theID)
 }
